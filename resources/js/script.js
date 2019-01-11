97af50bb41185a105b98e8d8d6d703ff8b73a52c
illlama <beatlemaniakih@gmail.com>
2019-01-11 23:35:19 +0900

    1.11 edited

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,40 +61,65 @@ $(document).ready(function() {
     /* Animations on scroll*/
     /*---------------------*/
     
-       $('.js--wp-5').waypoint(function(direction) {
+       $('.js-wp-market').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeInUp').css('opacity', '1.0');
     }, {
-        offset: '60%'
+        offset: '90%'
     });
-           $('.js--wp-5').waypoint(function(direction) {
+           $('.js-wp-market').waypoint(function(direction) {
         $('.js--wp-2').addClass('animated fadeInUp').css('opacity', '1.0');
     }, {
-        offset: '50%'
+        offset: '90%'
     });
-           $('.js--wp-5').waypoint(function(direction) {
+           $('.js-wp-market').waypoint(function(direction) {
         $('.js--wp-3').addClass('animated fadeInUp').css('opacity', '1.0');
     }, {
-        offset: '80%'
+        offset: '90%'
     });
-           $('.js--wp-5').waypoint(function(direction) {
+           $('.js-wp-market').waypoint(function(direction) {
         $('.js--wp-4').addClass('animated fadeInUp').css('opacity', '1.0');
     }, {
-        offset: '70%'
+        offset: '90%'
     });
-           $('.js--wp-5').waypoint(function(direction) {
+           $('.js-wp-market').waypoint(function(direction) {
         $('.js--wp-5').addClass('animated fadeInUp').css('opacity', '1.0');
     }, {
         offset: '90%'
     });
-           $('.js--wp-5').waypoint(function(direction) {
+           $('.js-wp-market').waypoint(function(direction) {
         $('.js--wp-6').addClass('animated fadeInUp').css('opacity', '1.0');
     }, {
-        offset: '80%'
+        offset: '90%'
     });
-           $('.js--wp-1').waypoint(function(direction) {
+           $('.js-wp-market').waypoint(function(direction) {
         $('.js--wp-7').addClass('animated fadeInUp').css('opacity', '1.0');
     }, {
-        offset: '10%'
+        offset: '90%'
+    });
+               $('.js-wp-market').waypoint(function(direction) {
+        $('.js--wp-8').addClass('animated fadeInUp').css('opacity', '1.0');
+    }, {
+        offset: '90%'
+    });
+               $('.js-wp-market').waypoint(function(direction) {
+        $('.js--wp-9').addClass('animated fadeInUp').css('opacity', '1.0');
+    }, {
+        offset: '90%'
+    });
+                 $('.js--wp-9').waypoint(function(direction) {
+        $('.js--wp-10').addClass('animated flipInX').css('opacity', '1.0');
+    }, {
+        offset: '90%'
+    });
+                $('.js--wp-c-0').waypoint(function(direction) {
+        $('.js--wp-11').addClass('animated flipInX').css('opacity', '1.0');
+    }, {
+        offset: '90%'
+    });
+                $('.js--wp-12-p').waypoint(function(direction) {
+        $('.js--wp-12').addClass('animated flipInX').css('opacity', '1.0');
+    }, {
+        offset: '90%'
     });
     
 //    Critic animation iphone
@@ -102,7 +127,7 @@ $(document).ready(function() {
     $('.js--wp-c-0').waypoint(function(direction) {
         $('.js--wp-c-1').addClass('animated fadeInUp').css('opacity', '1.0');
     }, {
-        offset: '30%'
+        offset: '80%'
     });
     
     
@@ -111,14 +136,22 @@ $(document).ready(function() {
     /*----------------------*/
     
 //    $('.js--wp-0').waypoint(function(direction) {
-//        var nav = $('.js--wp-1');
+//        var nav = $('.js--wp-3');
 //        
 //        if(nav.hasClass('animated fadeInUp')){
 //            console.log('hi');
-//            $('.js--wp-1, .js--wp-2, .js--wp-3, .js--wp-4, .js--wp-5, .js--wp-6, .js--wp-7, .js--wp-c-1').removeClass('animated fadeInUp').css('opacity', '0');
+//            $('.js--wp-1, .js--wp-2, .js--wp-3, .js--wp-4, .js--wp-5, .js--wp-6, .js--wp-7, .js--wp-8, .js--wp-9, .js--wp-c-1').removeClass('animated fadeInUp').css('opacity', '0');
 //        }   
 //    });
-    
+//    $('.js--wp-0').waypoint(function(direction) {
+//        var nav = $('.js--wp-10');
+//        
+//        if(nav.hasClass('animated flipInX')){
+//            console.log('hi');
+//            $('.js--wp-10, .js--wp-11, .js--wp-12').removeClass('animated flipInX').css('opacity', '0');
+//        }   
+//    });
+//    
     /*-------------------------*/
     /*CRITIC img change effects*/
     /*-------------------------*/
@@ -130,10 +163,11 @@ $(document).ready(function() {
     array[4] = "resources/css/img/img_critic3.png"
     array[5] = "resources/css/img/img_critic4.png"
     array[6] = "resources/css/img/img_critic5.png"
+    array[7] = "resources/css/img/img_critic6.png"
     
     i = 1;
     function changeValue(){
-    if (i <=5){
+    if (i <=6){
       i = i+1;
     }else{
       i = 1;
